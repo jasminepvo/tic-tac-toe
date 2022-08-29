87af84adbf30be28628e2831377abd53ae22f950
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-08-29 17:08:44 -0400

    Made working logic for the game that allows you to select whether you are player 1 or player 2 and click on boxes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+//Store all grid indexes into array
+let gridIndex = document.querySelectorAll(".grid-index");
 
+console.log(gridIndex);
+
+//Make players
+let player1 = document.querySelector(".player-1");
+let player2 = document.querySelector(".player-2");
+
+//Player 1
+player1.addEventListener("click", (event) => {
+	//Loop through indexes and allow player to click any box
+	for (let i = 0; i < gridIndex.length; i++) {
+		gridIndex[i].addEventListener("click", (event) => {
+			gridIndex[i].innerText = "X";
+		});
+	}
+});
+
+//player 2
+
+player2.addEventListener("click", (event) => {
+	console.log("I am player 2");
+
+	/*when player 2 clicks button, I want whenever they click a
+  box, for it to show O*/
+
+	for (let i = 0; i < gridIndex.length; i++) {
+		gridIndex[i].addEventListener("click", (event) => {
+			gridIndex[i].innerText = "O";
+		});
+	}
+
+	// gridIndex[0].addEventListener("click", (event) => {
+	//   gridIndex[0].innerText = "O";
+	// });
+});
