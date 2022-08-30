86fc605505d1844afe972c7b00654aea27eb304c
Kyle Christian <kylebolanos@gmail.com>
2022-08-30 16:45:11 -0700

    refactored again

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+let gridIndex = document.querySelectorAll(".grid-index");
+
+console.log(gridIndex);
+
 //remember, classes are syntactical sugar. They are fundamentally still functions, meaning that you can call them when needed.
 
 // selects player 1 button
@@ -8,57 +12,26 @@ class Player {
 	
 	choose() {
 		for (let i = 0; i < gridIndex.length; i++) {
-			gridIndex[i].addEventListener("click", (event) => {
-				gridIndex[i].querySelector('span').innerText = "X";
+			gridIndex[i].addEventListener("click", () => {
+				if (this.playerID === 'PLAYER 1') {
+					gridIndex[i].querySelector('span').innerText = "X";
+				} else {
+					gridIndex[i].querySelector('span').innerText = "O";
+				}
+				
 			});
 		}
 	}
 }
 
 
-document.querySelector('.btn').addEventListener('click', function() {
-	
-	let newPlayer = new Player(document.querySelector('.btn').innerText);
-	console.log(newPlayer);
+let buttons = document.querySelectorAll('.btn');
 
-	newPlayer.choose()
-});
-
-
-
-//Store all grid indexes into array
-let gridIndex = document.querySelectorAll(".grid-index");
-
-console.log(gridIndex);
-
-// //Make players
-// let player1 = document.querySelector(".player-1");
-// let player2 = document.querySelector(".player-2");
-
-// //Player 1
-// player1.addEventListener("click", (event) => {
-// 	player1.classList.add('button-clicked');
-// 	player2.classList.remove('button-clicked');
-
-
-// 	//Loop through indexes and allow player to click each box
-// 	for (let i = 0; i < gridIndex.length; i++) {
-// 		gridIndex[i].addEventListener("click", (event) => {
-// 			gridIndex[i].querySelector('span').innerText = "X";
-// 		});
-// 	}
-// });
-
-// //Player 2
-
-// player2.addEventListener("click", (event) => {
-// 	player1.classList.remove('button-clicked');
-// 	player2.classList.add('button-clicked');
-
-// 	for (let i = 0; i < gridIndex.length; i++) {
-// 		gridIndex[i].addEventListener("click", (event) => {
-// 			gridIndex[i].querySelector('span').innerText = "O";
-// 		});
-// 	}
-// });
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', function() {
+		let newPlayer = new Player(buttons[i].innerText);
+		console.log(newPlayer);
+		newPlayer.choose();
+	})
+}
 
