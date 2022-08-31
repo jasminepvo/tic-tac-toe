9fe6e772fe22b2937a8d955c419a9f0747910bc9
Kyle Christian <kylebolanos@gmail.com>
2022-08-30 19:19:48 -0700

    FULLY REFACTORED TO OOP WOOOOOOOOOOOOO!

--- js/main.js
+++ js/main.js
@@ -1,22 +1,64 @@
-let gridIndex = document.querySelectorAll(".grid-index");
+/* Logic Needed
 
-console.log(gridIndex);
+1. define the grid
 
-//remember, classes are syntactical sugar. They are fundamentally still functions, meaning that you can call them when needed.
+2. define buttons
 
-// selects player 1 button
-class Player {
-	constructor(playerID) {
-		this.playerID = playerID
-	}
-	
-	choose() {
-		for (let i = 0; i < gridIndex.length; i++) {
-			gridIndex[i].addEventListener("click", () => {
-				if (this.playerID === 'PLAYER 1') {
-					gridIndex[i].querySelector('span').innerText = "X";
+3. player class should target button innerText and have a choose() method for placing X's and O's
+
+
+I still think this code is spaghetti to the max but it's OOP!
+*/
+class Game {
+    constructor() {
+        this.gridIndex = document.querySelectorAll('.grid-index')
+        this.buttons = document.querySelectorAll('.btn')
+    }
+    
+    initButtons() {
+        for (let i = 0; i < this.buttons.length; i++) {
+
+            this.buttons[i].addEventListener("click", e => {
+                console.log(this.buttons[i].innerText);
+
+                //adds button-clicked class into css to simulate pressing a button
+                if (this.buttons[i] === this.buttons[0]) {
+                    this.buttons[0].classList.add('button-clicked');
+                    this.buttons[1].classList.remove('button-clicked');
+                } else {
+                    this.buttons[0].classList.remove('button-clicked');
+                    this.buttons[1].classList.add('button-clicked');
+                }
+
+                // uses the Player class to name the buttons after button names and initializes choose() so X's and O's appear on the dom
+                let newPlayer = new Player(this.buttons[i].innerText);
+                console.log(newPlayer);
+                newPlayer.choose();
+            })
+        }
+    }
+}
+
+class Player extends Game {
+    constructor(playerID) {
+        super()
+        super.gridIndex
+        this._playerID = playerID
+    }
+
+    //used to name the buttons
+    get playerID() {
+        return this._playerID
+    }
+    
+    //actual code for the X's and O's. The above just initializes the method.
+    choose() {
+		for (let i = 0; i < this.gridIndex.length; i++) {
+			this.gridIndex[i].addEventListener("click", () => {
+				if (this._playerID === 'PLAYER 1') {
+					this.gridIndex[i].querySelector('span').innerText = "X";
 				} else {
-					gridIndex[i].querySelector('span').innerText = "O";
+					this.gridIndex[i].querySelector('span').innerText = "O";
 				}
 				
 			});
@@ -24,14 +66,7 @@ class Player {
 	}
 }
 
+let init = new Game;
 
-let buttons = document.querySelectorAll('.btn');
-
-for (let i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener('click', function() {
-		let newPlayer = new Player(buttons[i].innerText);
-		console.log(newPlayer);
-		newPlayer.choose();
-	})
-}
+init.initButtons();
 
